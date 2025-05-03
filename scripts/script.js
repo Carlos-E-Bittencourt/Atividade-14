6d1354a56f0b9061500f0b87a31303f69ba43525
Carlos-E-Bittencourt <carlos_bittencourt2@estudante.sesisenai.org.br>
2025-05-03 17:46:36 -0300

    UPDATE - Terminei

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function openAbout(){
+    document.getElementById("about").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeAbout(){
+    document.getElementById("about").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
